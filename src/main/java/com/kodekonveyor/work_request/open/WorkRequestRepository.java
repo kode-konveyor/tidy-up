2cb1ccfcacc0f7c814a31aecb1c4616d312b6e30
Shubham Derhgawen <30868812+Shubhamd13@users.noreply.github.com>
2019-11-17 15:27:38 +0530

    Update WorkRequestRepository.java

--- src/main/java/com/kodekonveyor/work_request/open/WorkRequestRepository.java
+++ src/main/java/com/kodekonveyor/work_request/open/WorkRequestRepository.java
@@ -1,9 +1,10 @@
 package com.kodekonveyor.work_request.open;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WorkRequestRepository {
+public interface WorkRequestRepository extends CrudRepository<WorkRequestEntity, Long> {
 
 	WorkRequestEntity findByWorkRequestId(long workrequestId);
 
