210b48846a6849dcd6da8ce30d45505cd00201ee
JinLei324 <xiaojinteacher@gmail.com>
2020-01-16 21:16:43 +0800

    update

--- src/main/java/com/kodekonveyor/work_request/offer/GiveofferController.java
+++ src/main/java/com/kodekonveyor/work_request/offer/GiveofferController.java
@@ -1,16 +1,10 @@
 package com.kodekonveyor.work_request.offer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.kodekonveyor.work_request.WorkRequestRepository;
-
 @Controller
 public class GiveofferController {
-
-  @Autowired
-  private WorkRequestRepository workRequestRepository;
 
   @PostMapping("/offer")
 
