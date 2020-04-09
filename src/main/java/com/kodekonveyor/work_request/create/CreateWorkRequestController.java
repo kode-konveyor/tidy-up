6808251a347ca0815f087f3162c315a0dac9eb8c
Mayuresh94 <mayureshkutte@gmail.com>
2020-04-09 10:45:34 +0530

    Changes implemented

--- src/main/java/com/kodekonveyor/work_request/create/CreateWorkRequestController.java
+++ src/main/java/com/kodekonveyor/work_request/create/CreateWorkRequestController.java
@@ -1,6 +1,6 @@
-
 package com.kodekonveyor.work_request.create;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kodekonveyor.authentication.AuthenticatedUserService;
 import com.kodekonveyor.authentication.UserEntity;
 import com.kodekonveyor.authentication.UserEntityRepository;
+import com.kodekonveyor.webapp.LoggingConstants;
 import com.kodekonveyor.work_request.AddressDTO;
 import com.kodekonveyor.work_request.AddressEntity;
 import com.kodekonveyor.work_request.AddressUtil;
+import com.kodekonveyor.work_request.WorkRequestConstants;
 import com.kodekonveyor.work_request.WorkRequestEntity;
 import com.kodekonveyor.work_request.WorkRequestRepository;
 import com.kodekonveyor.work_request.WorkTypeEnum;
@@ -28,11 +30,20 @@ public class CreateWorkRequestController {
   @Autowired
   WorkTypeEnum workTypeEnum;
 
+  @Autowired
+  private Logger loggerService; //NOPMD
+
   @PostMapping("/work-request")
   public void
       call(@RequestBody final CreateWorkRequestDTO createWorkRequestDTO) {
+    loggerService.info(WorkRequestConstants.WORK_REQUEST_RECEIVED);
 
     WorkRequestValidationUtil.validateWorkRequest(createWorkRequestDTO);
+    loggerService.debug(
+        LoggingConstants.SUCCESS, LoggingConstants.INPUT_VALIDATION,
+        LoggingConstants.UNKNOWN
+
+    );
 
     createWorkRequest(createWorkRequestDTO);
 
@@ -54,6 +65,10 @@ public class CreateWorkRequestController {
     workRequestEntity.setDescription(createWorkRequestDTO.getDescription());
     workRequestEntity.setAddress(addressEntity);
     workRequestRepository.save(workRequestEntity);
+    loggerService.debug(
+        LoggingConstants.SUCCESS, LoggingConstants.WORK_REQUEST_ENTITY_CREATED,
+        workRequestEntity.getId()
+    );
 
   }
 
