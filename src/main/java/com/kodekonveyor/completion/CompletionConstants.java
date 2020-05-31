b630061b7cf28b3a1e0c1ed481892338dd01f90d
raghuvendra.pal <raghuvendra.pal@pearson.com>
2020-05-31 19:13:59 +0530

    Fix for #103 : MarkCompletionController/Logging

--- src/main/java/com/kodekonveyor/completion/CompletionConstants.java
+++ src/main/java/com/kodekonveyor/completion/CompletionConstants.java
@@ -5,4 +5,11 @@ public class CompletionConstants {
   public static final String MARK_AS_PAID_PATH = "/paid/{workRequestId}";
   public static final String MARK_AS_PAID_WORK_REQUEST_ID = "workRequestId";
 
+  public static final String LOG_API_CALL = "Api call name : {}, called by : {}";
+  public static final String LOG_API_CALL_STATUS = "Api call name : {},  status : {}";
+  public static final String LOG_API_CALL_FALURE_STATUS = "Api call name : {}, status : {}, error : {}";
+  public static final String SUCCESS = "SUCCESS";
+  public static final String FAILURE = "FAILURE";
+  public static final String INVALID_WORK_REQUEST_STATUS = "Invalid work status!";
+
 }
--- src/test/java/com/kodekonveyor/completion/MarkCompletionControllerTestBase.java
+++ src/test/java/com/kodekonveyor/completion/MarkCompletionControllerTestBase.java
@@ -1,11 +1,14 @@
 package com.kodekonveyor.completion;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import com.kodekonveyor.work_request.WorkRequestEntityRepositoryStubs;
 import com.kodekonveyor.work_request.WorkRequestRepository;
+import org.slf4j.Logger;
 
 public class MarkCompletionControllerTestBase {
 
@@ -13,6 +16,10 @@ public class MarkCompletionControllerTestBase {
   MarkCompletionController markCompletionController;
   @Mock
   WorkRequestRepository workRequestRepository;
+  @Mock
+  Logger loggerService;
+  @Captor
+  ArgumentCaptor<String> captorString;
 
   @BeforeEach
   void setUp() {
